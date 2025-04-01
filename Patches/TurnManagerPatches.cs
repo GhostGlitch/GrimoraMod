77998b68378d2e454cb6685713f15fdc95b47148
Ghost <taylerobin@gmail.com>
2025-04-01 16:47:45 -0600

    Fixed Kaycee's mod softlock better.

--- Patches/TurnManagerPatches.cs
+++ Patches/TurnManagerPatches.cs
@@ -18,61 +18,60 @@ public class TurnManagerPatches
 	)
 	{
 		yield return enumerator;
-		if (GrimoraRunState.CurrentRun == null) {
-			Log.LogDebug("[GrimoraMod] Skipping SetupPhase additions – CurrentRun is null.");
-			yield break;
-		}
 		if (ConfigHelper.Instance.IsDevModeEnabled)
 		{
 			yield return ResourcesManager.Instance.AddBones(25);
 		}
-
-		bool drawPile3DIsActive = CardDrawPiles3D.Instance && CardDrawPiles3D.Instance.pile;
-
-		if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_StartingDraw"))
-		{
-			if (drawPile3DIsActive)
+		if (GrimoraRunState.CurrentRun != null) {
+			bool drawPile3DIsActive = CardDrawPiles3D.Instance && CardDrawPiles3D.Instance.pile;
+	
+			if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_StartingDraw"))
 			{
-				CardDrawPiles3D.Instance.pile.Draw();
-				yield return CardDrawPiles.Instance.DrawCardFromDeck();
+				if (drawPile3DIsActive)
+				{
+					CardDrawPiles3D.Instance.pile.Draw();
+					yield return CardDrawPiles.Instance.DrawCardFromDeck();
+				}
 			}
-		}
-
-		if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_MaxEnergy"))
-		{
-			yield return ResourcesManager.Instance.AddMaxEnergy(1);
-			yield return ResourcesManager.Instance.AddEnergy(1);
-		}
-
-		if (SaveFile.IsAscension && AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.EasyGuards) && TurnManager.Instance.Opponent is BaseBossExt)
-		{
-			yield return ResourcesManager.Instance.AddBones(1);
-			yield return ResourcesManager.Instance.AddMaxEnergy(1);
-			yield return ResourcesManager.Instance.AddEnergy(1);
-
-		}
-
-		if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_BossBones") && TurnManager.Instance.Opponent is BaseBossExt)
-		{
-
-			yield return ResourcesManager.Instance.AddBones(3);
-
-		}
-
-		if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_TerrainSpawn")) yield return BoardManager.Instance.GetPlayerOpenSlots().GetRandomItem().CreateCardInSlot(NameDeadTree.GetCardInfo());
-
-		if (!SaveFile.IsAscension ||( SaveFile.IsAscension && !AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.NoBones)))
-		{
-			int bonesToAdd = GrimoraRunState.CurrentRun.regionTier;
-			if (GrimoraRunState.CurrentRun.riggedDraws != null) bonesToAdd -= GrimoraRunState.CurrentRun.riggedDraws.Count();
-			Log.LogDebug($"[SetupPhase] Adding [{bonesToAdd}] bones");
-			yield return ResourcesManager.Instance.AddBones(Math.Max(bonesToAdd, 0));
-		}
-		else
-		{
-			//only show activation when No Bones is changing something
-			if (AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.NoBones) && GrimoraRunState.CurrentRun.regionTier > 0) ChallengeActivationUI.TryShowActivation(ChallengeManagement.NoBones);
-			Log.LogInfo($"{SaveFile.IsAscension } +  {AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.NoBones)}");
+	
+			if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_MaxEnergy"))
+			{
+				yield return ResourcesManager.Instance.AddMaxEnergy(1);
+				yield return ResourcesManager.Instance.AddEnergy(1);
+			}
+	
+			if (SaveFile.IsAscension && AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.EasyGuards) && TurnManager.Instance.Opponent is BaseBossExt)
+			{
+				yield return ResourcesManager.Instance.AddBones(1);
+				yield return ResourcesManager.Instance.AddMaxEnergy(1);
+				yield return ResourcesManager.Instance.AddEnergy(1);
+	
+			}
+	
+			if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_BossBones") && TurnManager.Instance.Opponent is BaseBossExt)
+			{
+	
+				yield return ResourcesManager.Instance.AddBones(3);
+	
+			}
+	
+			if (GrimoraRunState.CurrentRun.riggedDraws.Contains("Boon_TerrainSpawn")) yield return BoardManager.Instance.GetPlayerOpenSlots().GetRandomItem().CreateCardInSlot(NameDeadTree.GetCardInfo());
+	
+			if (!SaveFile.IsAscension ||( SaveFile.IsAscension && !AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.NoBones)))
+			{
+				int bonesToAdd = GrimoraRunState.CurrentRun.regionTier;
+				if (GrimoraRunState.CurrentRun.riggedDraws != null) bonesToAdd -= GrimoraRunState.CurrentRun.riggedDraws.Count();
+				Log.LogDebug($"[SetupPhase] Adding [{bonesToAdd}] bones");
+				yield return ResourcesManager.Instance.AddBones(Math.Max(bonesToAdd, 0));
+			}
+			else
+			{
+				//only show activation when No Bones is changing something
+				if (AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.NoBones) && GrimoraRunState.CurrentRun.regionTier > 0) ChallengeActivationUI.TryShowActivation(ChallengeManagement.NoBones);
+				Log.LogInfo($"{SaveFile.IsAscension } +  {AscensionSaveData.Data.ChallengeIsActive(ChallengeManagement.NoBones)}");
+			}
+		} else {
+			Log.LogInfo("[SetupPhase] Skipping Grimora-specific logic – no active Grimora run.");
 		}
 	}
 
